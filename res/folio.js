c1cdd251eb9ce56de78cfecf713e298181444c14
Raul Illana <rfi@acronyms.agency>
2024-11-14 15:55:52 +0100

    add portfolio link to home and fix folio colors and data

--- res/folio.js
+++ res/folio.js
@@ -14,7 +14,7 @@ const createArticle = (data) => {
     // Create image container with fixed aspect ratio (800/600 = 1.33)
     const imageContainer = document.createElement('div');
     imageContainer.className = 'image-container';
-    imageContainer.style.paddingBottom = '33.33%';
+    imageContainer.style.paddingBottom = '40%';
     
     const thumbnail = document.createElement('img');
     thumbnail.alt = data.url;
@@ -30,7 +30,11 @@ const createArticle = (data) => {
     
     const type = document.createElement('span');
     type.textContent = data.type;
-    type.className = `badge ${data.type.toLowerCase()}`;
+    type.className = `badge type ${data.type.toLowerCase()}`;
+
+    const tech = document.createElement('span');
+    tech.textContent = data.tech;
+    tech.className = `badge tech ${data.tech.toLowerCase()}`;
     
     const title = document.createElement('h2');
     const titleLink = document.createElement('a');
@@ -40,9 +44,10 @@ const createArticle = (data) => {
     titleLink.rel = 'noopener noreferrer';
     title.appendChild(titleLink);
     
+    article.appendChild(title);
     article.appendChild(imageContainer);
     article.appendChild(type);
-    article.appendChild(title);
+    article.appendChild(tech);
     
     return article;
 };
@@ -63,97 +68,94 @@ const observeImages = () => {
     });
 };
 
-// Deprecated projects:
-//{ type: "FREELANCE", url: "https://socialistes.cat" },
-
 const projects = [
-    { type: "PERSONAL", url: "https://embotellador.es" },
-    { type: "PERSONAL", url: "https://alegre.pro" },
-    { type: "PERSONAL", url: "https://raulillana.com" },
-    { type: "FREELANCE", url: "https://cooperatingvolunteers.com" },
-    { type: "AGENCY", url: "https://unareceta.com" },
-    { type: "AGENCY", url: "https://museunacional.cat" },
-    { type: "AGENCY", url: "https://pcactual.com" },
-    { type: "AGENCY", url: "https://rbarevistas.com" },
-    { type: "AGENCY", url: "https://agorasanitaria.com/" },
-    { type: "AGENCY", url: "https://cofbserveis.net/" },
-    { type: "AGENCY", url: "https://ddgi.cat/web/" },
-    { type: "AGENCY", url: "https://miriadax.net/" },
-    { type: "AGENCY", url: "https://smartcampus.neurotrauma.net/" },
-    { type: "AGENCY", url: "https://moodleted.telefonicaed.com/" },
-    { type: "AGENCY", url: "https://theycorrect.com/" },
-    { type: "AGENCY", url: "https://wiikon.es/" },
-    { type: "AGENCY", url: "https://campus-mcmutual.com/" },
-    { type: "AGENCY", url: "https://geze.es/" },
-    { type: "AGENCY", url: "https://irec.cat/" },
-    { type: "AGENCY", url: "https://actualidadviajes.com/" },
-    { type: "AGENCY", url: "https://absolutcruceros.com/" },
-    { type: "AGENCY", url: "https://tatuantes.com/" },
-    { type: "AGENCY", url: "https://notigatos.es/" },
-    { type: "AGENCY", url: "https://noticaballos.com/" },
-    { type: "FREELANCE", url: "https://coptering.com" },
-    { type: "FREELANCE", url: "https://kalapa-clinic.com" },
-    { type: "FREELANCE", url: "https://misterbarcelo.com" },
-    { type: "FREELANCE", url: "https://felycan.es" },
-    { type: "FREELANCE", url: "https://sensalialabs.com" },
-    { type: "FREELANCE", url: "https://stakehound.com" },
-    { type: "FREELANCE", url: "https://imperfectus.com" },
-    { type: "PERSONAL", url: "https://iphoneado.com" },
-    { type: "PERSONAL", url: "https://paradiabeticos.es" },
-    { type: "PERSONAL", url: "https://wpmercs.com" },
-    { type: "PERSONAL", url: "https://wordcamp.es" },
-    { type: "AGENCY", url: "https://mundoperros.es/" },
-    { type: "AGENCY", url: "https://meteorologiaenred.com/" },
-    { type: "AGENCY", url: "https://hombresconestilo.com/" },
-    { type: "AGENCY", url: "https://cibercactus.com/" },
-    { type: "AGENCY", url: "https://actualidadliteratura.com/" },
-    { type: "AGENCY", url: "https://renovablesverdes.com/" },
-    { type: "AGENCY", url: "https://formacionyestudios.com/" },
-    { type: "AGENCY", url: "https://economiafinanzas.com/" },
-    { type: "AGENCY", url: "https://actualidadecommerce.com/" },
-    { type: "AGENCY", url: "https://nutridieta.com/" },
-    { type: "AGENCY", url: "https://madreshoy.com/" },
-    { type: "AGENCY", url: "https://jardineriaon.com/" },
-    { type: "AGENCY", url: "https://thermorecetas.com/" },
-    { type: "AGENCY", url: "https://mycookrecetas.com/" },
-    { type: "AGENCY", url: "https://windowsnoticias.com/" },
-    { type: "AGENCY", url: "https://ubunlog.com/" },
-    { type: "AGENCY", url: "https://trucosdescargas.com/" },
-    { type: "AGENCY", url: "https://todoereaders.com/" },
-    { type: "AGENCY", url: "https://soydemac.com/" },
-    { type: "AGENCY", url: "https://linuxadictos.com/" },
-    { type: "AGENCY", url: "https://hwlibre.com/" },
-    { type: "AGENCY", url: "https://blog.desdelinux.net/" },
-    { type: "AGENCY", url: "https://creativosonline.org/blog/" },
-    { type: "AGENCY", url: "https://androidguias.com/" },
-    { type: "AGENCY", url: "https://androidsis.com/" },
-    { type: "AGENCY", url: "https://actualidadiphone.com/" },
-    { type: "AGENCY", url: "https://actualidadgadget.com" },
-    { type: "AGENCY", url: "https://actualidadmotor.com" },
-    { type: "AGENCY", url: "https://guiaswow.com" },
-    { type: "AGENCY", url: "https://bezzia.com" },
-    { type: "AGENCY", url: "https://lasrecetascocina.com" },
-    { type: "AGENCY", url: "https://hoteles4you.com" },
-    { type: "AGENCY", url: "https://absolutviajes.com" },
-    { type: "AGENCY", url: "https://actualidadblog.com" },
-    { type: "AGENCY", url: "https://abinternet.es" },
-    { type: "AGENCY", url: "https://crenco.org/" },
-    { type: "AGENCY", url: "https://dracoglobal.es/" },
-    { type: "AGENCY", url: "https://eduma.com/" },
-    { type: "AGENCY", url: "https://escolamarilocasals.com/" },
-    { type: "AGENCY", url: "https://fp.uoc.fje.edu/ca/estudis" },
-    { type: "AGENCY", url: "https://folio.uoc.edu/" },
-    { type: "AGENCY", url: "https://uoc.edu/portal/es/uoc-x/" },
-    { type: "AGENCY", url: "https://didactaplus.com/" },
-    { type: "AGENCY", url: "https://mirai.com" },
-    { type: "AGENCY", url: "https://almirall.com" },
-    { type: "AGENCY", url: "https://almirall.es" },
-    { type: "AGENCY", url: "https://almirall.co.uk" },
-    { type: "AGENCY", url: "https://almirall.global" },
-    { type: "AGENCY", url: "https://terribilis.net" },
-    { type: "AGENCY", url: "https://blog.camon.es" },
-    { type: "AGENCY", url: "https://iasist.com.es" },
-    { type: "AGENCY", url: "https://gestion.campus-mcmutual.com/" }
+    { tech: "WORDPRESS", type: "PERSONAL", url: "https://embotellador.es" },
+    { tech: "WORDPRESS", type: "PERSONAL", url: "https://alegre.pro" },
+    { tech: "STATIC", type: "PERSONAL", url: "https://raulillana.com" },
+    { tech: "WORDPRESS", type: "FREELANCE", url: "https://cooperatingvolunteers.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://unareceta.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://museunacional.cat" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://pcactual.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://rbarevistas.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://agorasanitaria.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://cofbserveis.net/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://ddgi.cat/web/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://miriadax.net/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://smartcampus.neurotrauma.net/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://moodleted.telefonicaed.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://theycorrect.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://wiikon.es/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://campus-mcmutual.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://geze.es/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://irec.cat/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://actualidadviajes.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://absolutcruceros.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://tatuantes.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://notigatos.es/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://noticaballos.com/" },
+    { tech: "WORDPRESS", type: "FREELANCE", url: "https://coptering.com" },
+    { tech: "WORDPRESS", type: "FREELANCE", url: "https://kalapa-clinic.com" },
+    { tech: "WORDPRESS", type: "FREELANCE", url: "https://misterbarcelo.com" },
+    { tech: "WORDPRESS", type: "FREELANCE", url: "https://felycan.es" },
+    { tech: "WORDPRESS", type: "FREELANCE", url: "https://sensalialabs.com" },
+    { tech: "WORDPRESS", type: "FREELANCE", url: "https://stakehound.com" },
+    { tech: "WORDPRESS", type: "FREELANCE", url: "https://imperfectus.com" },
+    { tech: "WORDPRESS", type: "PERSONAL", url: "https://iphoneado.com" },
+    { tech: "WORDPRESS", type: "PERSONAL", url: "https://paradiabeticos.es" },
+    { tech: "WORDPRESS", type: "PERSONAL", url: "https://wpmercs.com" },
+    { tech: "WORDPRESS", type: "PERSONAL", url: "https://wordcamp.es" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://mundoperros.es/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://meteorologiaenred.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://hombresconestilo.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://cibercactus.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://actualidadliteratura.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://renovablesverdes.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://formacionyestudios.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://economiafinanzas.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://actualidadecommerce.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://nutridieta.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://madreshoy.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://jardineriaon.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://thermorecetas.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://mycookrecetas.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://windowsnoticias.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://ubunlog.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://trucosdescargas.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://todoereaders.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://soydemac.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://linuxadictos.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://hwlibre.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://blog.desdelinux.net/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://creativosonline.org/blog/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://androidguias.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://androidsis.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://actualidadiphone.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://actualidadgadget.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://actualidadmotor.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://guiaswow.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://bezzia.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://lasrecetascocina.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://hoteles4you.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://absolutviajes.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://actualidadblog.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://abinternet.es" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://crenco.org/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://dracoglobal.es/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://eduma.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://escolamarilocasals.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://fp.uoc.fje.edu/ca/estudis" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://folio.uoc.edu/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://uoc.edu/portal/es/uoc-x/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://didactaplus.com/" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://mirai.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://almirall.com" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://almirall.es" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://almirall.co.uk" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://almirall.global" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://terribilis.net" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://blog.camon.es" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://iasist.com.es" },
+    { tech: "WORDPRESS", type: "AGENCY", url: "https://gestion.campus-mcmutual.com/" }
 ];
 
 const loadFolio = () => {
